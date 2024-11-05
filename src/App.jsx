5ded94c3c617a5f41ffe43c00b9e408f0a7759ea
Luiz Filipe <99333746+Filipee01@users.noreply.github.com>
2024-11-05 12:12:58 -0300

    add some changes

---
+++ src/App.jsx
@@ -0,0 +1,75 @@
+import './App.css'
+import { useState, useEffect } from 'react'
+
+function App() {
+  const [products, setProducts] = useState([])
+  const url = 'http://localhost:3001/products'
+
+  const [name, setName] = useState('')
+  const [price, setPrice] = useState('')
+
+  //resgate de dados da api
+  useEffect(() => {
+    async function fetchData() {
+    fetch(url)
+    .then(response => response.json())
+    .then(data => setProducts(data))
+    }
+    fetchData()
+  },[])
+
+ // adicao de um novo produto
+ const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const product = {
+      name,
+      price,
+    }
+
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(product)
+    })
+
+    //carregamento dinamico de produtos
+    const addedProduct = await res.json()
+    setProducts(prevProducts => [...prevProducts, addedProduct])
+
+    setName('')
+    setPrice('')
+
+ }
+
+  return (
+    <div className='app'>
+    <h1>lista de produtos</h1>
+    <ul>
+      {products.map(product => (
+        <li key={product.id}>
+          <h2>{product.name}</h2>
+          <p>{product.price}</p>
+        </li>
+      ))}
+    </ul>
+    <div className="add-product">
+      <form onSubmit={handleSubmit}>
+        <label>
+          Nome:
+          <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        </label>
+        <label>
+          Preço:
+          <input type="number" value={price} onChange={(e) => setPrice(e.target.value)} />
+        </label>
+        <button type="submit">Adicionar</button>
+      </form>
+    </div>
+    </div>
+  )
+}
+
+export default App
